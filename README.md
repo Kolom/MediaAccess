69c4647bb087c96ef3ff90ea24b30265eb6cb8f2
Ваня Колом <kolomiyets.ivan.s@gmail.com>
2016-11-14 18:34:50 +0200

    success error field fix, proj doc

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
 ## MediaAccess
+
 This is Media Access responsive mobile friendly form (for customer).
 
-
-## Stack 
-Jade 
-Less 
-Gulp 
-Bootstrap3
-Javascript 
+|  Stack  |
+|----------|
+| Jade   |
+| Less  |
+| Gulp  |
+| Bootstrap3 |
+| plain Javascript |
 
 ## Features
--Form fields validation;
--Password validation with real time color strength indicator;
--Custom checkboxes;
--Display popup after submitting a form, if all fields validation passed correctly.
+* Form fields validation; 
+* Password validation with real time color strength indicator; 
+* Custom checkboxes; 
+* Display popup after submitting a form, if all fields validation passed correctly.
 
 ## Quickstart
 
-$ npm install
-$ npm start (or 'gulp')
-$ start localhost:8080/index.html
+    $ git clone https://github.com/Kolom/MediaAccess
+    $ cd MediaAccess    
+    $ sudo npm install gulp -g && npm install 
+    $ npm start 
+    $ start localhost:8080/index.html
--- source/js/index.js
+++ source/js/index.js
@@ -4,17 +4,23 @@
  * Initialize form event listeners
  */
 function initForm() {
-    var accountForm = document.querySelector('.new_account_form--customer');
-    var passwordInput = document.querySelector('#password-input');
+  var accountForm = document.querySelector('.new_account_form--customer');
+  var passwordInput = document.querySelector('#password-input');
 
-    accountForm.onsubmit = submitAccountForm; // set onsubmit event handler for form
-    accountForm.noValidate = true; //disable browser validation
+  accountForm.onsubmit = submitAccountForm; // set onsubmit event handler for form
+  accountForm.noValidate = true; //disable browser validation
 
-    passwordInput.oninput = checkPassStrength; // set oninput event handler for password input
+  passwordInput.oninput = checkPassStrength; // set oninput event handler for password input
+
+  // hide check boxes if URL contains "country" or "countries" somewhere in path
+  var checkBoxesSection = document.querySelector('.choose_country');
+  if ((/country|countries/).test(window.location.href)) checkBoxesSection.classList.add('hidden');
+
+  console.log(window.location.href);
 }
 
 window.onload = function () {
-    initForm();
+  initForm();
 };
 
 /* --- Event handlers section --- */
@@ -25,19 +31,19 @@ window.onload = function () {
  * @returns {boolean} - form common validation result
  */
 function submitAccountForm() {
-    var account = getAccountObj();
-    var accountValidationResult = validateAccount(account);
+  var account = getAccountObj();
+  var accountValidationResult = validateAccount(account);
 
-    //check that all fields are valid
-    if (accountValidationResult.isValid === true) {
-        var popup = document.querySelector('#popup');
-        popup.classList.remove('hidden'); //show popup
-    } else
-    //show validation messages if some of form fields aren't valid
-        renderValidationMessages(accountValidationResult);
+  //check that all fields are valid
+  if (accountValidationResult.isValid === true) {
+    var popup = document.querySelector('#popup');
+    popup.classList.remove('hidden'); //show popup
+  } else
+  //show validation messages if some of form fields aren't valid
+    renderValidationMessages(accountValidationResult);
 
-    // return false to prevent page reload
-    return false;
+  // return false to prevent page reload
+  return false;
 }
 
 /**
@@ -46,30 +52,30 @@ function submitAccountForm() {
  * @param e - input event
  */
 function checkPassStrength(e) {
-    console.log(e);
-    var passLength = e.target.value.length;
+  console.log(e);
+  var passLength = e.target.value.length;
 
-    //get indicators
-    var firstLevelStrength = document.querySelector('.pass-str-indicator > .first_level');
-    var secondLevelStrength = document.querySelector('.pass-str-indicator > .sec_level');
-    var thirdLevelStrength = document.querySelector('.pass-str-indicator > .third_level');
-    var fourthLevelStrength = document.querySelector('.pass-str-indicator > .fourth_level');
+  //get indicators
+  var firstLevelStrength = document.querySelector('.pass-str-indicator > .first_level');
+  var secondLevelStrength = document.querySelector('.pass-str-indicator > .sec_level');
+  var thirdLevelStrength = document.querySelector('.pass-str-indicator > .third_level');
+  var fourthLevelStrength = document.querySelector('.pass-str-indicator > .fourth_level');
 
-    //hide all
-    firstLevelStrength.classList.add('hidden');
-    secondLevelStrength.classList.add('hidden');
-    thirdLevelStrength.classList.add('hidden');
-    fourthLevelStrength.classList.add('hidden');
+  //hide all
+  firstLevelStrength.classList.add('hidden');
+  secondLevelStrength.classList.add('hidden');
+  thirdLevelStrength.classList.add('hidden');
+  fourthLevelStrength.classList.add('hidden');
 
-    //show appropriate indicator
-    if (passLength < 6)
-        firstLevelStrength.classList.remove('hidden');
-    else if (passLength < 8)
-        secondLevelStrength.classList.remove('hidden');
-    else if (passLength < 9)
-        thirdLevelStrength.classList.remove('hidden');
-    else
-        fourthLevelStrength.classList.remove('hidden');
+  //show appropriate indicator
+  if (passLength < 6)
+    firstLevelStrength.classList.remove('hidden');
+  else if (passLength < 8)
+    secondLevelStrength.classList.remove('hidden');
+  else if (passLength < 9)
+    thirdLevelStrength.classList.remove('hidden');
+  else
+    fourthLevelStrength.classList.remove('hidden');
 }
 
 /* --- Validation --- */
@@ -81,76 +87,46 @@ function checkPassStrength(e) {
  * @returns {{email: {isValid: boolean, validationMessage: string}, fName: {isValid: boolean, validationMessage: string}, lName: {isValid: boolean, validationMessage: string}, password: {isValid: boolean, validationMessage: string}, confirmPassword: {isValid: boolean, validationMessage: string}, isValid: boolean}}
  */
 function validateAccount(account) {
-    //validation result object default state
-    var validationResult = {
-        email: {
-            isValid: false,
-            validationMessage: "please, type in valid e-mail!"
-        },
-        fName: {
-            isValid: false,
-            validationMessage: "please, type in valid first name!"
-        },
-        lName: {
-            isValid: false,
-            validationMessage: "please, type in valid last name!"
-        },
-        password: {
-            isValid: false,
-            validationMessage: "please, type in valid password!"
-        },
-        confirmPassword: {
-            isValid: false,
-            validationMessage: "confirm password must met password!"
-        },
-        isValid: false
-    };
+  //validation result object default state
+  var validationResult = {
+    email: {
+      isValid: false,
+      validationMessage: "please, type in valid e-mail!"
+    },
+    fName: {
+      isValid: false,
+      validationMessage: "please, type in valid first name!"
+    },
+    lName: {
+      isValid: false,
+      validationMessage: "please, type in valid last name!"
+    },
+    password: {
+      isValid: false,
+      validationMessage: "please, type in valid password!"
+    },
+    confirmPassword: {
+      isValid: false,
+      validationMessage: "confirm password must met password!"
+    },
+    isValid: false
+  };
 
-    //validate email
-    validationResult.email.isValid = (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(account.email);
-    //validate first name
-    validationResult.fName.isValid = account.fName.trim().length > 0;
-    //validate last name
-    validationResult.lName.isValid = account.lName.trim().length > 0;
-    //validate password
-    validationResult.password.isValid = (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,50}$/).test(account.password);
-    //validate confirm password
-    validationResult.confirmPassword.isValid = (account.password === account.confirmPassword);
+  //validate email
+  validationResult.email.isValid = (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(account.email);
+  //validate first name
+  validationResult.fName.isValid = account.fName.trim().length > 0;
+  //validate last name
+  validationResult.lName.isValid = account.lName.trim().length > 0;
+  //validate password
+  validationResult.password.isValid = (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,50}$/).test(account.password);
+  //validate confirm password
+  validationResult.confirmPassword.isValid = (account.password === account.confirmPassword);
 
-    //whole form validate
-    validationResult.isValid = validationResult.email.isValid && validationResult.fName.isValid && validationResult.lName.isValid && validationResult.password.isValid && validationResult.confirmPassword.isValid
+  //whole form validate
+  validationResult.isValid = validationResult.email.isValid && validationResult.fName.isValid && validationResult.lName.isValid && validationResult.password.isValid && validationResult.confirmPassword.isValid
 
-    return validationResult;
-}
-
-
-/**
- * Render validation messages under form fields
- *
- * @param validationResult - object represents form fields validation result
- */
-function renderValidationMessages(validationResult) {
-    console.log(validationResult);
-
-    var emailErrorP = document.querySelector('.input-error--email');
-    var fNameErrorP = document.querySelector('.input-error--f_name');
-    var lNameErrorP = document.querySelector('.input-error--l_name');
-    var passwordErrorP = document.querySelector('.input-error--password');
-    var passConfirmationP = document.querySelector('.pass_confirmation > .input-error--password');
-
-    //set validation messages
-    emailErrorP.innerHTML = validationResult.email.validationMessage;
-    fNameErrorP.innerHTML = validationResult.fName.validationMessage;
-    lNameErrorP.innerHTML = validationResult.lName.validationMessage;
-    passwordErrorP.innerHTML = validationResult.password.validationMessage;
-    passConfirmationP.innerHTML = validationResult.confirmPassword.validationMessage;
-
-    //show or hide validation messages refer to isValid flag
-    if (validationResult.email.isValid) emailErrorP.classList.add('hidden'); else emailErrorP.classList.remove('hidden');
-    if (validationResult.fName.isValid) fNameErrorP.classList.add('hidden'); else fNameErrorP.classList.remove('hidden');
-    if (validationResult.lName.isValid) lNameErrorP.classList.add('hidden'); else lNameErrorP.classList.remove('hidden');
-    if (validationResult.password.isValid) passwordErrorP.classList.add('hidden'); else passwordErrorP.classList.remove('hidden');
-    if (validationResult.confirmPassword.isValid) passConfirmationP.classList.add('hidden'); else passConfirmationP.classList.remove('hidden');
+  return validationResult;
 }
 
 /**
@@ -159,18 +135,86 @@ function renderValidationMessages(validationResult) {
  * @returns {{email: *, fName: *, lName: *, password: *, confirmPassword: *}}
  */
 function getAccountObj() {
-    var emailInput = document.querySelector('#email-input');
-    var fNameInput = document.querySelector('#first_name-input');
-    var lNameInput = document.querySelector('#last_name-input');
-    var passwordInput = document.querySelector('#password-input');
-    var confirmPasswordInput = document.querySelector('#password_confirm-input');
+  var emailInput = document.querySelector('#email-input');
+  var fNameInput = document.querySelector('#first_name-input');
+  var lNameInput = document.querySelector('#last_name-input');
+  var passwordInput = document.querySelector('#password-input');
+  var confirmPasswordInput = document.querySelector('#password_confirm-input');
 
-    return {
-        email: emailInput.value,
-        fName: fNameInput.value,
-        lName: lNameInput.value,
-        password: passwordInput.value,
-        confirmPassword: confirmPasswordInput.value
-    }
+  return {
+    email: emailInput.value,
+    fName: fNameInput.value,
+    lName: lNameInput.value,
+    password: passwordInput.value,
+    confirmPassword: confirmPasswordInput.value
+  }
 }
 
+/**
+ * Render validation messages under form fields
+ *
+ * @param validationResult - object represents form fields validation result
+ */
+function renderValidationMessages(validationResult) {
+  /**
+   * Add .success, remove .error from validated <input/>
+   * hide error <p/> message
+   *
+   * @param input
+   * @param p
+   */
+  function successValidation(input, p) {
+    input.classList.add('success');
+    input.classList.remove('error');
+    p.classList.add('hidden');
+  }
+
+  /**
+   * Add .error, remove .success from validated <input/>
+   * show error <p/> message
+   *
+   * @param input
+   * @param p
+   */
+  function errorValidation(input, p) {
+    input.classList.remove('success');
+    input.classList.add('error');
+    p.classList.remove('hidden');
+  }
+
+  //<input/>
+  var emailInput = document.querySelector('#email-input');
+  var fNameInput = document.querySelector('#first_name-input');
+  var lNameInput = document.querySelector('#last_name-input');
+  var passwordInput = document.querySelector('#password-input');
+  var passConfirmationInput = document.querySelector('#password_confirm-input');
+
+  //<p/>
+  var emailErrorP = document.querySelector('.input-error--email');
+  var fNameErrorP = document.querySelector('.input-error--f_name');
+  var lNameErrorP = document.querySelector('.input-error--l_name');
+  var passwordErrorP = document.querySelector('.input-error--password');
+  var passConfirmationP = document.querySelector('.pass_confirmation > .input-error--password');
+
+  //set validation messages
+  emailErrorP.innerHTML = validationResult.email.validationMessage;
+  fNameErrorP.innerHTML = validationResult.fName.validationMessage;
+  lNameErrorP.innerHTML = validationResult.lName.validationMessage;
+  passwordErrorP.innerHTML = validationResult.password.validationMessage;
+  passConfirmationP.innerHTML = validationResult.confirmPassword.validationMessage;
+
+  /**
+   * Show or hide validation messages refer to isValid flag
+   * Add appropriate class - .error of .success to inputs
+   */
+  //email
+  if (validationResult.email.isValid) successValidation(emailInput, emailErrorP); else errorValidation(emailInput, emailErrorP);
+  //first name
+  if (validationResult.fName.isValid) successValidation(fNameInput, fNameErrorP); else errorValidation(fNameInput, fNameErrorP);
+  //last name
+  if (validationResult.lName.isValid) successValidation(lNameInput, lNameErrorP); else errorValidation(lNameInput, lNameErrorP);
+  //password
+  if (validationResult.password.isValid) successValidation(passwordInput, passwordErrorP); else errorValidation(passwordInput, passwordErrorP);
+  //confirm password
+  if (validationResult.confirmPassword.isValid) successValidation(passConfirmationInput, passConfirmationP); else errorValidation(passConfirmationInput, passConfirmationP);
+}
